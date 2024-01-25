7c727f0068f29a0be2af71ead324fee3dbb423d2
Prince <princekashishbbu@gmail.com>
2024-01-25 15:28:06 +0530

    Todolist delet buttojn is missing

--- app/page.js
+++ app/page.js
@@ -1,5 +1,10 @@
 "use client";
 import React, { useState } from "react";
+import {
+  RiCheckboxBlankCircleLine,
+  RiCheckboxCircleFill,
+} from "react-icons/ri";
+import { MdDelete } from "react-icons/md";
 
 function page() {
   const [task, settask] = useState("");
@@ -15,20 +20,40 @@ function page() {
     setDesc("");
     console.log(add);
   };
+  const deleteHandler = (i) => {
+    let copyarray = [...add];
+    copyarray.splice(i, 1);
+    setDadd(copyarray);
+  };
+
   let rander = <h1 className=" text-rose-500">*No dask assign </h1>;
 
-if(add.length>0){
-  rander = add.map((x) => {
-    return (
-      <li className="bg-gray-100 text-black min-w-[560px] rounded-md">
-        <div className=" flex justify-around items-center gap-8">
-        <h5 className="font-semibold text-xl ">{x.task}</h5>
-        <h6 className="font-base text-lg">{x.Desc}</h6>
-      </div>
-      </li>
-    );
-  });
-}
+  if (add.length > 0) {
+    rander = add.map((x, i) => {
+      return (
+        <li
+          key={i}
+          className="bg-gray-100 text-black min-w-[560px] flex rounded-md mt-4"
+        >
+          <div className=" flex justify-between w-full items-center gap-8">
+            <div className="flex m-3 gap-4">
+              <input type="checkbox" />
+              <h5 className="font-semibold text-xl ">{x.task}</h5>
+            </div>
+            <h6 className="font-base text-lg">{x.Desc}</h6>
+            <MdDelete
+              onClick={() => {
+                deleteHandler(i);
+              }}
+              size={25}
+              className="flex justify-end hover:shadow-orange-400 cursor-pointer items-end"
+            />
+          </div>
+        </li>
+      );
+    });
+  }
+
   return (
     <>
       <div className="bg-black flex flex-col  items-center justify-center h-screen  p-5 shadow-xl">
@@ -38,7 +63,7 @@ if(add.length>0){
             <input
               type="text"
               placeholder="Task "
-              className="border-2 border-white rounded "
+              className="border-2 border-white h-10 text-lg w-64 rounded "
               value={task}
               onChange={(e) => {
                 settask(e.target.value);
@@ -47,18 +72,18 @@ if(add.length>0){
             <input
               type="text"
               placeholder="Desciption"
-              className="border-2 ml-5 border-white rounded text-black "
+              className="border-2 ml-5 border-white h-10 w-64 text-lg rounded text-black "
               value={Desc}
               onChange={(e) => {
                 setDesc(e.target.value);
               }}
             />
-            <button className="bg-orange-400 px-8  text-white rounded-lg ml-4 py-2">
+            <button className="bg-orange-400 px-9  text-white hover:bg-orange-600 font-semibold rounded-lg ml-4 py-2">
               Add
             </button>
           </form>
         </div>
-        <div className="  flex justify-around" >
+        <div className="  flex justify-around">
           <ul>
             <li className="">{rander}</li>
           </ul>
